82b5d52effb45976ed34908e97fffb47a1fef1f3
Kristian Hetherington <hetheringtonkristian@gmail.com>
2020-05-18 11:06:11 +0200

    two identical divs with different images and headers

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render("Hello World!", document.getElementById("react-app"));
+import "./index.css";
+
+const appContent = (
+  <div className="app">
+    <div className="frame">
+      <h1 className="frame__header">Some nice picture of a cat</h1>
+
+      <div className="frame__content">
+        <img src="img/cat_1.jpeg"></img>
+      </div>
+    </div>
+
+    <div className="frame">
+      <h1 className="frame__header">Anonymous cat</h1>
+
+      <div className="frame__content">
+        <img src="img/cat_2.jpeg"></img>
+      </div>
+    </div>
+  </div>
+);
+const rootElement = document.getElementById("react-app");
+
+ReactDOM.render(appContent, rootElement);
