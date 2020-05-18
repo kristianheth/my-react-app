ca5e68db9b34d9dd26469a292359f3f02d43be37
Kristian Hetherington <hetheringtonkristian@gmail.com>
2020-05-18 13:56:10 +0200

    added props into JSX

--- src/index.js
+++ src/index.js
@@ -3,23 +3,34 @@ import ReactDOM from "react-dom";
 
 import "./index.css";
 
+const cats = [
+  {
+    headerText: "Some nice picture of a cat",
+    imageSrc: "./img/cat_1.jpeg",
+  },
+  {
+    headerText: "Anonymous cat",
+    imageSrc: "./img/cat_2.jpeg",
+  },
+];
+
+const FrameForImage = (props) => {
+  return (
+    <div className="frame">
+      <h1 className="frame__header">{props.header}</h1>
+
+      <div className="frame__content">
+        <img src={props.image} />
+      </div>
+    </div>
+  );
+};
+
 const appContent = (
   <div className="app">
-    <div className="frame">
-      <h1 className="frame__header">Some nice picture of a cat</h1>
+    <FrameForImage header={cats[0].headerText} image={cats[0].imageSrc} />
 
-      <div className="frame__content">
-        <img src="./img/cat_1.jpeg" alt="cute kitten" />
-      </div>
-    </div>
-
-    <div className="frame">
-      <h1 className="frame__header">Anonymous cat</h1>
-
-      <div className="frame__content">
-        <img src="img/cat_2.jpeg" alt="cute kitten"></img>
-      </div>
-    </div>
+    <FrameForImage header={cats[1].headerText} image={cats[1].imageSrc} />
   </div>
 );
 const rootElement = document.getElementById("react-app");
